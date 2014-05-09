2097d29a41c26974c9040be42d510d5857e48f7b
Matthew Lai <matlai17@gmail.com>
2014-05-09 11:06:59 -0700

    Seventh Commit. Added some comments to MMR

--- src/MMR.java
+++ src/MMR.java
@@ -14,6 +14,13 @@ import java.util.TreeSet;
  * Documents and Producing Summaries". Allows ranking of documents, not only by their 
  * relevance to a query, but by their marginality to other relevant documents.
  * 
+ * To add a new Similarity Metric, assign it a unique integer identification
+ * and name as a "static final int" variable. This new identification does not 
+ * needed to be added the the SimMetric interface file. Then add the new metric to the 
+ * second constructor in the Switch-Case using the new similarity metric's 
+ * identification as the case statement. Then simply initialize the MMR class
+ * using the new metric's identification.
+ * 
  * @author Matthew Lai
  */
 public class MMR {
@@ -76,10 +83,22 @@ public class MMR {
         }
     }
     
+    /**
+     * Returns a ranked list given the a list of documents, a query, and a lambda value.
+     * The list will be ranked according to the lambda value. A lambda value of 0 will
+     * return a list ranked more on diversity while a lambda value of 1 will return a 
+     * standard relevance ranked list. It is recommended the lambda value be between 
+     * 0 and 1.
+     * 
+     * @param documentList Documents stored in a list as a list of Strings, representing its words
+     * @param query A query represented as a list of Strings, representing query words
+     * @param lambda The lambda value to determine MMR search ranking
+     * @param maxResults The maximum size of the returned resulting ranked list.
+     * @return The list of ranked sentences, ranked according to the lambda value
+     */
     public List<List<String>> rankedList(List<List<String>> documentList, List<String> query, double lambda, int maxResults)
     {
         TreeSet<Document_Score> sList = new TreeSet<>(new Comparator<Document_Score>() {
-
             @Override
             public int compare(Document_Score o1, Document_Score o2) {
                 if(o1.getScore() == o2.getScore()) return 1;
@@ -106,6 +125,17 @@ public class MMR {
         return rankedList;
     }
     
+    /**
+     * Private method that returns a singe document that has the maximum score based
+     * on the its relevance to the query and its marginality compared to the 
+     * sentences already retrieved.
+     * 
+     * @param unselectedDocuments A list of documents that will be considered for selection
+     * @param selectedDocuments The list of documents already selected
+     * @param query The query against which the list of documents will be compared
+     * @param lambda The value which will determine relevance versus marginality
+     * @return A Document and Score tuple of the highest ranking sentence
+     */
     private Document_Score retrieveDocument(List<List<String>> unselectedDocuments, TreeSet<Document_Score> selectedDocuments, List<String> query, double lambda)
     {
         List<String> bestDocument = unselectedDocuments.get(0);
@@ -122,6 +152,15 @@ public class MMR {
         return new Document_Score(bestDocument, maxScore);
     }
     
+    /**
+     * Private method that is used by retrieveDocument to iterate through the selected
+     * documents and returns the score for the document with the greatest similarity
+     * to the considered document
+     * 
+     * @param unselectedDocument The document against which all selected documents will be compared against
+     * @param selectedDocuments The list of documents against which the considered document will be compared
+     * @return The greatest similarity score between the selected documents and the considered document.
+     */
     private double maxSim2(List<String> unselectedDocument, TreeSet<Document_Score> selectedDocuments)
     {
         double maxScore = 0;
@@ -135,6 +174,9 @@ public class MMR {
     }
     
     public static void main(String[] args) {
+        
+// Current Test Case is not a very good one
+        
         ArrayList<String> document1 = new ArrayList<>();
         document1.add("one");
         document1.add("two");
