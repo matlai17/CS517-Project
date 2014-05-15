a8b63e804da2bbbf2f7644a38dee12eb98a61486
Matthew Lai <matlai17@gmail.com>
2014-05-14 19:09:49 -0700

    Eighth Commit. Added better test cases. Can now much more easily see that the MMR algorithm works. Change the lambda value to test.

--- src/SimMetrics/CosineSim.java
+++ src/SimMetrics/CosineSim.java
@@ -67,61 +67,30 @@ public class CosineSim implements SimMetric {
     }
     
     public static void main(String[] args) {
-        ArrayList<String> document = new ArrayList<>();
+        ArrayList<ArrayList<String>> documents = new ArrayList<>();
         ArrayList<String> query = new ArrayList<>();
         
-        document.add("I");
-        document.add("like");
-        document.add("to");
-        document.add("eat");
-        document.add("pie");
-        document.add("for");
-        document.add("breakfast");
-        document.add("lunch");
-        document.add("and");
-        document.add("dinner");
+        String documentText[] = { "abcdef" , "wxyz" };
+        String queryText = "abcdzyx";
         
-// This block of query statement should get near 1        
-//        query.add("I");
-//        query.add("like");
-//        query.add("to");
-//        query.add("eat");
-//        query.add("pie");
-//        query.add("for");
-//        query.add("breakfast");
-//        query.add("lunch");
-//        query.add("and");
-//        query.add("dinner");
-
-// This block of query statement should get higher than 0
-        query.add("can");
-        query.add("I");
-        query.add("get");
-        query.add("pie");
-        query.add("for");
-        query.add("dinner");
-
-// This block of query statement should get higher than the one above       
-//        query.add("like");
-//        query.add("pie");
-//        query.add("breakfast");
-//        query.add("lunch");
-//        query.add("dinner");
+        for (int i = 0; i < documentText.length; i++) {
+            ArrayList<String> document = new ArrayList<>();
+            for(int j = 0; j < documentText[i].length(); j++){
+                document.add(documentText[i].charAt(j) + "");
+            }
+            documents.add(document);
+        }
         
-// This block of query statement should get lower than the one above       
-//        query.add("john");        
-//        query.add("like");
-//        query.add("pie");
-//        query.add("breakfast");
-//        query.add("lunch");
-//        query.add("dinner");
-
-// This block of query statement should get 0        
-//        query.add("whats");
-//        query.add("up");
-//        query.add("doc");
+        for(int i = 0; i < queryText.length(); i++) query.add(queryText.charAt(i) + "");
         
-        System.out.println(new CosineSim().documentRank(document, query));
+        for(ArrayList<String> document : documents)
+        {
+            for(String word : document) System.out.print(word + " ");
+            System.out.print("\t");
+            for(String word : query) System.out.print(word + " ");
+            System.out.print(" : \t");
+            System.out.println(new CosineSim().documentRank(document, query));
+        }
     }
     
 }
